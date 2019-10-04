ed1f1795ba87dad7dc3cd8bbade6aebb906cd2c9
Kai Peterson <pete9372@umn.edu>
2019-10-04 13:22:41 -0500

    got delete button to work and total meetups to increment/decrement

--- client.js
+++ client.js
@@ -1,11 +1,11 @@
 $(document).ready(readyNow);
 
 let index = 0;
-
+let otherIndex = 0;
 
 function readyNow() {
     $('#addMeetup').on('click', insertEvent)
-    $('.delete').on('click', deleteEvent);
+    $('#addMeetup').on('click', incrementTotal)
 }
 
 
@@ -17,10 +17,17 @@ function insertEvent() {
     let group = $('#group').val();
     let date = $('#date').val();
     let newMeetup = $('<tr ' + rowClass + '><td>' + group + '</td><td>' + date + '</td><td><button ' + buttonClass + '>Delete</button></td></tr>')
-    $('.tableBody').append(newMeetup); 
+    $('.tableBody').append(newMeetup);
+    $('.button' + otherIndex).on('click', function() {
+        otherIndex--;
+        $('h3').text('Total Meetups: ' + otherIndex);
+    });
+    $('.button' + otherIndex).on('click', function() {
+        $(this).closest('tr').remove();
+    })
 }
 
-function deleteEvent() {
-    console.log('.' + index);
-    $('.row' + index).remove();
+function incrementTotal() {
+    otherIndex++;
+    $('h3').text('Total Meetups: ' + otherIndex);
 }
